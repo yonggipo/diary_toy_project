0e887c05a2aff29b35758b65abc94feaaff9611d
yonggipo <yonggipo@icloud.com>
2024-07-09 03:39:53 +0900

    feat: split weather constants from text constants

--- lib/text.dart
+++ lib/text.dart
@@ -1,4 +1,4 @@
-class Text {
+final class Text {
   // Nav Items
   static const String diaryMainNavTitle = '내 다이어리';
   static const String diaryDetailNavTitle = '내 다이어리';
@@ -16,10 +16,15 @@ class Text {
   static const String diaryTitleTFPH = '작성하기';
   static const String diaryContentTFPH = '작성하기';
 
-  static class Weather {
-  static const String weatherCloud = '흐림';
-  static const String weatherSunny = '화창함';
-  static const String weatherRain = '비';
-  static const String weatherHot = '더움';
-  }
+  static const Weather weather = Weather();
+}
+
+// Weather Text
+final class Weather {
+  const Weather();
+
+  static const String cloud = '흐림';
+  static const String sunny = '화창함';
+  static const String rain = '비';
+  static const String hot = '더움';
 }
