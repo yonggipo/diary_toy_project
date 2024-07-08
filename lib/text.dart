19a6f00b1a8d7d092aff8d0ef69beac94e70526b
yonggipo <yonggipo@icloud.com>
2024-07-08 22:00:09 +0900

    feat: add string constants

---
+++ lib/text.dart
@@ -0,0 +1,25 @@
+class Text {
+  // Nav Items
+  static const String diaryMainNavTitle = '내 다이어리';
+  static const String diaryDetailNavTitle = '내 다이어리';
+
+  // Tab Bar Item
+  static const String firstTabBarItemTitle = '콘서트';
+  static const String secondTabBarItemTitle = '뒷풀이';
+  static const String thirdTabBarItemTitle = '현장나눔';
+
+  // Button Title
+  static const String writeDiray = '작성하기';
+  static const String saveDiray = '작성완료';
+
+  // TextField Placeholder
+  static const String diaryTitleTFPH = '작성하기';
+  static const String diaryContentTFPH = '작성하기';
+
+  static class Weather {
+  static const String weatherCloud = '흐림';
+  static const String weatherSunny = '화창함';
+  static const String weatherRain = '비';
+  static const String weatherHot = '더움';
+  }
+}
