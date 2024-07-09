c5326da6f4fc73dec1069d998d5d1e198d5e76e3
yonggipo <yonggipo@icloud.com>
2024-07-09 20:50:47 +0900

    feat: add notification_preferences entity & domain

---
+++ lib/layers/data/notification_preferences_dto.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:diary_toy_project/layers/domain/notification_preferences.dart';
+
+class NotificationPreferencesDTO extends NotificationPreferences {
+  NotificationPreferencesDTO({
+    super.email,
+    super.push,
+  });
+
+  factory NotificationPreferencesDTO.fromRawJson(String str) =>
+      NotificationPreferencesDTO.fromMap(json.decode(str));
+
+  String toRawJson() => json.encode(toMap());
+
+  factory NotificationPreferencesDTO.fromMap(Map<String, dynamic> json) =>
+      NotificationPreferencesDTO(
+        email: json['email'],
+        push: json['push'],
+      );
+
+  Map<String, dynamic> toMap() => {
+        'email': email,
+        'push': push,
+      };
+
+  NotificationPreferences toDomain() {
+    return this as NotificationPreferences;
+  }
+}
