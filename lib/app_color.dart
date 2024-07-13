2bd4833b0e17aa53e97131134469730f2007dfa3
yonggipo <yonggipo@icloud.com>
2024-07-13 21:34:52 +0900

    feat: add app color constants

--- lib/app_color.dart
+++ lib/app_color.dart
@@ -1,7 +1,16 @@
 import 'package:diary_toy_project/color_utils.dart';
 
 final class AppColor {
-  static final placeholder = ColorUtils.fromHex('ABABAB');
-  static final gray3 = ColorUtils.fromHex('EDEDED');
   static final label = ColorUtils.fromHex('717171');
+  static final placeholder = ColorUtils.fromHex('ABABAB');
+  static final border = ColorUtils.fromHex('DDDDDD');
+  static final backgroundColor = ColorUtils.fromHex('EDEDED');
+  static final black = ColorUtils.fromHex('000000');
+  static final button = ColorUtils.fromHex('CCCCCC');
+
+  /// Tag: Segmented Control
+  static final selectedColor = ColorUtils.fromHex('FFFFFF');
+  static final unSelectedColor = ColorUtils.fromHex('5F5F5F');
+  static final selectedBackgroundColor = ColorUtils.fromHex('969696');
+  static final unSelectedBackgroundColor = ColorUtils.fromHex('D9D9D9');
 }
