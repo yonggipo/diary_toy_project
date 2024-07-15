7725451b2ab177934ab7cbb243c024f24b6705de
yonggipo <yonggipo@icloud.com>
2024-07-16 02:56:43 +0900

    feat: add fetch detail api

--- lib/layers/data/source/network/api.dart
+++ lib/layers/data/source/network/api.dart
@@ -8,6 +8,7 @@ import '../../dto/diary/diary_dto.dart';
 
 abstract class API {
   Future<List<Diary>> fetchDiaries({int page = 0});
+  Future<Diary> fetchDetail({required String entryID});
 }
 
 final class DefaultAPI implements API {
@@ -20,8 +21,8 @@ final class DefaultAPI implements API {
   Future<List<Diary>> fetchDiaries({int page = 0}) async {
     try {
       // final Response<Map<String, dynamic>> response = await dio.post('https://handler-ciqgffjs6q-du.a.run.app/diary/list?user_id=iVEsnXvKgQOXe8GxjozT');
-      final Response<dynamic> response = await dio.post(
-          '${Secrets.baseURL}diary/list?user_id=iVEsnXvKgQOXe8GxjozT');
+      final Response<dynamic> response = await dio
+          .post('${Secrets.baseURL}diary/list?user_id=iVEsnXvKgQOXe8GxjozT');
       Map<String, dynamic> dataMap = json.decode(response.data);
       // final l = (response.data!['entries'] as List<dynamic>)
       final l = (dataMap['entries'] as List<dynamic>)
@@ -40,5 +41,35 @@ final class DefaultAPI implements API {
     }
 
     return [];
+  }
+
+  // MARK: Fetch Detail
+
+  @override
+  Future<Diary> fetchDetail({required String entryID}) async {
+    try {
+      // final Response<Map<String, dynamic>> response = await dio.post('https://handler-ciqgffjs6q-du.a.run.app/diary/?entry_id=HVOMzzggPqFnB7utAG4N');
+      final Response<dynamic> response = await dio
+          .post('${Secrets.baseURL}diary/?entry_id=HVOMzzggPqFnB7utAG4N');
+      Map<String, dynamic> dataMap = json.decode(response.data);
+      // final l = (response as dynamic)
+      final l = (dataMap as dynamic).map((e) => DiaryDTO.fromMap(e));
+      return l;
+    } on DioException catch (e) {
+      if (e.response != null) {
+        print('StatusCode: ${e.response?.statusCode}');
+        print('Data: ${e.response?.data}');
+        print('Headers: ${e.response?.headers}');
+        (e.response?.statusCode == 404)
+            ? throw Exception('Diary not found')
+            : throw Exception('Network Error: ${e.error}');
+      } else {
+        print('Unexpected error: $e');
+        rethrow;
+      }
+    } catch (e) {
+      print('Unexpected error: $e');
+      rethrow;
+    }
   }
 }
