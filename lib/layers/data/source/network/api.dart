e8821b31e46e3046ecf82150797f6147ce4f24b7
yonggipo <yonggipo@icloud.com>
2024-07-16 03:29:26 +0900

    feat: add diary add & update & delete api

--- lib/layers/data/source/network/api.dart
+++ lib/layers/data/source/network/api.dart
@@ -7,18 +7,21 @@ import '../../../domain/diary/diary.dart';
 import '../../dto/diary/diary_dto.dart';
 
 abstract class API {
-  Future<List<Diary>> fetchDiaries({int page = 0});
-  Future<Diary> fetchDetail({required String entryID});
+  Future<List<Diary>> list({int page = 0});
+  Future<Diary> detail({required String entryID});
+  Future<Diary> update({required String entryID, required Diary entry});
+  Future<Diary> add({required Diary entry});
+  Future<Diary> delete({required String entryID});
 }
 
 final class DefaultAPI implements API {
   final dio = Dio();
 
-  // MARK: Fetch List
+  // MARK: Fetch Diary List
 
   @override
   // Future<List<Diary>> fetchDiaries({int page = 0}) async {
-  Future<List<Diary>> fetchDiaries({int page = 0}) async {
+  Future<List<Diary>> list({int page = 0}) async {
     try {
       // final Response<Map<String, dynamic>> response = await dio.post('https://handler-ciqgffjs6q-du.a.run.app/diary/list?user_id=iVEsnXvKgQOXe8GxjozT');
       final Response<dynamic> response = await dio
@@ -43,10 +46,10 @@ final class DefaultAPI implements API {
     return [];
   }
 
-  // MARK: Fetch Detail
+  // MARK: Fetch Diary Detail
 
   @override
-  Future<Diary> fetchDetail({required String entryID}) async {
+  Future<Diary> detail({required String entryID}) async {
     try {
       // final Response<Map<String, dynamic>> response = await dio.post('https://handler-ciqgffjs6q-du.a.run.app/diary/?entry_id=HVOMzzggPqFnB7utAG4N');
       final Response<dynamic> response = await dio
@@ -55,6 +58,100 @@ final class DefaultAPI implements API {
       // final l = (response as dynamic)
       final l = (dataMap as dynamic).map((e) => DiaryDTO.fromMap(e));
       return l;
+    } on DioException catch (e) {
+      if (e.response != null) {
+        print('StatusCode: ${e.response?.statusCode}');
+        print('Data: ${e.response?.data}');
+        print('Headers: ${e.response?.headers}');
+        (e.response?.statusCode == 404)
+            ? throw Exception('Diary not found')
+            : throw Exception('Network Error: ${e.error}');
+      } else {
+        print('Unexpected error: $e');
+        rethrow;
+      }
+    } catch (e) {
+      print('Unexpected error: $e');
+      rethrow;
+    }
+  }
+
+  // MARK: Update Diary
+
+  @override
+  Future<Diary> update({
+    required String entryID,
+    required Diary entry,
+  }) async {
+    try {
+      // final Response<Map<String, dynamic>> response = await dio.put('${Secrets.baseURL}diary/?entry_id=$entryID');
+      final Response<dynamic> response = await dio.put(
+        '${Secrets.baseURL}diary/?entry_id=$entryID',
+        data: (entry as DiaryDTO).toJson(),
+      );
+      Map<String, dynamic> dataMap = json.decode(response.data);
+      // (response as dynamic)
+      return (dataMap as dynamic).map((e) => DiaryDTO.fromMap(e));
+    } on DioException catch (e) {
+      if (e.response != null) {
+        print('StatusCode: ${e.response?.statusCode}');
+        print('Data: ${e.response?.data}');
+        print('Headers: ${e.response?.headers}');
+        (e.response?.statusCode == 404)
+            ? throw Exception('Diary not found')
+            : throw Exception('Network Error: ${e.error}');
+      } else {
+        print('Unexpected error: $e');
+        rethrow;
+      }
+    } catch (e) {
+      print('Unexpected error: $e');
+      rethrow;
+    }
+  }
+
+  // MARK: Add Diary
+
+  @override
+  Future<Diary> add({required Diary entry}) async {
+    try {
+      // final Response<Map<String, dynamic>> response = await dio.post('${Secrets.baseURL}diary/create');
+      final Response<dynamic> response = await dio.post(
+       '${Secrets.baseURL}diary/create',
+        data: (entry as DiaryDTO).toJson(),
+      );
+      Map<String, dynamic> dataMap = json.decode(response.data);
+      // (response as dynamic)
+      return (dataMap as dynamic).map((e) => DiaryDTO.fromMap(e));
+    } on DioException catch (e) {
+      if (e.response != null) {
+        print('StatusCode: ${e.response?.statusCode}');
+        print('Data: ${e.response?.data}');
+        print('Headers: ${e.response?.headers}');
+        (e.response?.statusCode == 404)
+            ? throw Exception('Diary not found')
+            : throw Exception('Network Error: ${e.error}');
+      } else {
+        print('Unexpected error: $e');
+        rethrow;
+      }
+    } catch (e) {
+      print('Unexpected error: $e');
+      rethrow;
+    }
+  }
+
+  // MARK: Delete Diary
+
+  @override
+  Future<Diary> delete({required String entryID}) async {
+    try {
+      // final Response<Map<String, dynamic>> response = await dio.delete('https://handler-ciqgffjs6q-du.a.run.app/diary/?entry_id=HVOMzzggPqFnB7utAG4N');
+      final Response<dynamic> response = await dio
+          .delete('${Secrets.baseURL}diary/?entry_id=$entryID');
+      Map<String, dynamic> dataMap = json.decode(response.data);
+      // final l = (response as dynamic)
+      return (dataMap as dynamic).map((e) => DiaryDTO.fromMap(e));
     } on DioException catch (e) {
       if (e.response != null) {
         print('StatusCode: ${e.response?.statusCode}');
