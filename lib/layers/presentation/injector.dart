ba69869661a7b2c0da911012169c91159c75945a
yonggipo <yonggipo@icloud.com>
2024-07-16 07:39:36 +0900

    feat: add injector

---
+++ lib/layers/presentation/injector.dart
@@ -0,0 +1,44 @@
+import 'package:diary_toy_project/layers/domain/usecase/diary_usecase.dart';
+import 'package:diary_toy_project/layers/presentation/diary_list/controller/list_page_controller.dart';
+import 'package:get_it/get_it.dart';
+
+import '../data/default_diary_repository.dart';
+import '../data/source/network/api.dart';
+import '../domain/repository/diary_repository.dart';
+
+GetIt getIt = GetIt.instance;
+
+Future<void> initializeGetIt() async {
+
+  // MARK: - Data Layer
+
+  getIt.registerLazySingleton<API>(() => DefaultAPI());
+  // getIt.registerFactory<LocalStorage>(
+  //   () => LocalStorageImpl(
+  //     sharedPreferences: sharedPref,
+  //   ),
+  // );
+
+  getIt.registerFactory<DiaryRepository>(
+    () => DefaultDiaryRepository(
+      api: getIt(),
+    ),
+  );
+
+  // MARK: - Domain Layer
+
+  getIt.registerFactory(
+    () => DiaryUsecase(
+      repository: getIt(),
+    ),
+  );
+
+  // MARK: - Presentation Layer
+
+  getIt.registerLazySingleton(
+    () => ListPageController(diaryUsecase: getIt()),
+  );
+  // getIt.registerLazySingleton(
+  //   () => CharacterDetailsController(),
+  // );
+}
