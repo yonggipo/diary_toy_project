417ef3b907c8652fb36ff37c883fe9300787280b
yonggipo <yonggipo@icloud.com>
2024-07-16 07:38:23 +0900

    feat: add loding_indicator & segmented_control and delete previous period_button

---
+++ lib/layers/presentation/shared/segmented_control.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+typedef ValueChanged<T> = void Function(T value);
+
+final class SegmentedControl<T extends Object> extends StatefulWidget {
+  const SegmentedControl({
+    super.key,
+    required this.values,
+    required this.onValueChanged,
+    this.selectedColor = Colors.black,
+    this.unSelectedColor = Colors.black,
+    this.selectedBackgroundColor = Colors.grey,
+    this.unSelectedBackgroundColor = Colors.white,
+  });
+
+  final List<T> values;
+  final ValueChanged<T> onValueChanged;
+  final Color selectedColor;
+  final Color unSelectedColor;
+  final Color selectedBackgroundColor;
+  final Color unSelectedBackgroundColor;
+
+  @override
+  State<SegmentedControl<T>> createState() => _SegmentedControlState<T>();
+}
+
+final class _SegmentedControlState<T extends Object>
+    extends State<SegmentedControl<T>> {
+  late T _currentValue;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentValue = widget.values.first;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        for (int i = 0; i < widget.values.length; i++) ...[
+          ValueButton(
+            title: widget.values[i].toString(),
+            onPressed: () {
+              setState(() {
+                _currentValue = widget.values[i];
+                widget.onValueChanged(_currentValue);
+              });
+            },
+            selectedColor: widget.selectedColor,
+            unSelectedColor: widget.unSelectedColor,
+            selectedBackgroundColor: widget.selectedBackgroundColor,
+            unSelectedBackgroundColor: widget.unSelectedBackgroundColor,
+            isSelected: widget.values[i] == _currentValue,
+          ),
+          if (i < widget.values.length - 1) const Gap(4.0),
+        ],
+      ],
+    );
+  }
+}
+
+final class ValueButton extends StatelessWidget {
+  const ValueButton({
+    super.key,
+    required this.title,
+    required this.onPressed,
+    required this.selectedColor,
+    required this.unSelectedColor,
+    required this.selectedBackgroundColor,
+    required this.unSelectedBackgroundColor,
+    required this.isSelected,
+  });
+
+  final String title;
+  final VoidCallback onPressed;
+  final Color selectedColor;
+  final Color unSelectedColor;
+  final Color selectedBackgroundColor;
+  final Color unSelectedBackgroundColor;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: TextButton.styleFrom(
+        textStyle: TextStyle(
+          fontSize: 12,
+          fontWeight: isSelected ? FontWeight.bold : FontWeight.w300,
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        foregroundColor: isSelected ? selectedColor : unSelectedColor,
+        backgroundColor:
+            isSelected ? selectedBackgroundColor : unSelectedBackgroundColor,
+      ),
+      onPressed: onPressed,
+      child: Text(title),
+    );
+  }
+}
