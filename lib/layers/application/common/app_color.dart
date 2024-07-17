932a67ae2378cf1d895e6df47adfe0e609315659
yonggipo <yonggipo@icloud.com>
2024-07-17 22:59:32 +0900

    refact: refactoring color class

--- lib/layers/application/common/app_color.dart
+++ lib/layers/application/common/app_color.dart
@@ -1,16 +1,16 @@
-import 'package:diary_toy_project/layers/application/utils/color_utils.dart';
+import 'dart:ui';
 
 final class AppColor {
-  static final label = ColorUtils.fromHex('717171');
-  static final placeholder = ColorUtils.fromHex('ABABAB');
-  static final border = ColorUtils.fromHex('DDDDDD');
-  static final backgroundColor = ColorUtils.fromHex('EDEDED');
-  static final black = ColorUtils.fromHex('000000');
-  static final button = ColorUtils.fromHex('CCCCCC');
+  static const label = Color(0xFF717171);
+  static const placeholder = Color(0xFFABABAB);
+  static const border = Color(0xFFDDDDDD);
+  static const backgroundColor = Color(0xFFEDEDED);
+  static const black = Color(0xFF000000);
+  static const button = Color(0xFFCCCCCC);
 
   /// Tag: Segmented Control
-  static final selectedColor = ColorUtils.fromHex('FFFFFF');
-  static final unSelectedColor = ColorUtils.fromHex('5F5F5F');
-  static final selectedBackgroundColor = ColorUtils.fromHex('969696');
-  static final unSelectedBackgroundColor = ColorUtils.fromHex('D9D9D9');
+  static const selectedColor = Color(0xFFFFFFFF);
+  static const unSelectedColor = Color(0xFF5F5F5F);
+  static const selectedBackgroundColor = Color(0xFF969696);
+  static const unSelectedBackgroundColor = Color(0xFFD9D9D9);
 }
